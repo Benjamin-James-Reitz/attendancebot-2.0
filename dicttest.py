0573d01ffd5fe990e96e8285dcacbb3cae30f529
Benjamin-James-Reitz <benjamin.james.reitz@protonmail.com>
2022-04-03 18:44:44 -0400

    works sans email

--- dicttest.py
+++ dicttest.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
-from ast import Num
-from asyncore import loop
-from operator import contains
+# from ast import Num
+# from asyncore import loop
+# from operator import contains
 # from lib2to3.pytree import _Results
 from re import X
 import sys, csv
 from csv import reader
-import pandas as pd
-from tkinter import N
-from unicodedata import name
+# import pandas as pd
+# from tkinter import N
+# from unicodedata import name
 #from optparse import Values
 #from unicodedata import name
 
@@ -21,7 +21,7 @@ email_report = ""
 word_search = str(22)
 seniors = []
 everyone_else = []
-# line_count = 0
+
 #read csv, and split on "," the line
 file = open("attendance_test.csv", newline='')
 csvreader = csv.reader(file, delimiter=',')
@@ -30,19 +30,7 @@ rows = []
 for row in csvreader:
     rows.append(row)
 line_count = (len(rows))
-#  file_name = 'attendance_test.csv'
-# def count_lines_readlines(file_name):
-#     fp = open(file_name,'r')
-#     line_count = len(fp.readlines())
-#     return line_count
-#     return line_count
-# print (line_count)
-# print(results)
 
-# with open('attendance_test.csv', newline='') as csvfile:
-#     read_csv = csv.reader(csvfile, delimiter=",")
-#     value = len(list(read_csv))
-#     print (value)
 #create two new lists: seniors and non-seniors
 #first, initialize the lists
 file = open("attendance_test.csv")
@@ -51,47 +39,58 @@ header = next(csvreader)
 while line_count >= 1:
     for row in rows:
         if word_search in row[1]:
-            print("Found! This one is a senior: ", row)
+            # print("Found! This one is a senior: ", row)
             seniors.append(row)
             line_count -= line_count 
         else:
-            print("Not found! This one is NOT a senior: ", row)
+            # print("Not found! This one is NOT a senior: ", row)
             everyone_else.append(row)
             line_count -= line_count 
 
 # everyone_else.remove(everyone_else[0])
-print ("seniors list: ", seniors)
-print ("non-seniors list: ", everyone_else)
+# print ("seniors list: ", seniors)
+# print ("non-seniors list: ", everyone_else)
 
 #set the variables list item 0=name, 1=email address 2=atnum, 
-# 3=skipnum, 4=attrate, 5=remaining_number of sessions, 6=still need to attend 
+# 3=skipnum, 4=attrate, 5=remaining_number of sessions, 6=still need to attend 7=email message
 attrate = 0
 remaining_number_sessions_seniors = 0
 neednum_seniors = 0
 neednum_everyone_else = 0
 remaining_dates_seniors = [
-4/4, 4/5, 4/6, 4/7, 
-4/11, 4/12, 4/13, 4/14, 
-4/18, 4/19, 4/20, 4/21, 
-4/25, 4/26, 4/27, 4/28,
-5/2, 5/3, 5/4, 5/5,
-5/9, 5/10, 5/11, 5/12
+'4-4', '4-5', '4-6', '4-7', 
+'4-11', '4-12', '4-13', '4-14', 
+'4-18', '4-19', '4-20', '4-21', 
+'4-25', '4-26', '4-27', '4-28',
+'5-2', '5-3', '5-4', '5-5'
 ] 
+print(remaining_dates_seniors)
 remaining_number_sessions_seniors = len(remaining_dates_seniors)
-print ("number of sessions left for seniors: ", len(remaining_dates_seniors))
+remaining_dates_sessions_seniors_string=''
+remaining_dates_sessions_seniors_string=''.join([str(item) + ", " for item in remaining_dates_seniors])
+# print ("number of sessions left for seniors: ", len(remaining_dates_seniors))
+remaining_dates_sessions_seniors_string_stripped=remaining_dates_sessions_seniors_string[:-2]
+print(remaining_dates_sessions_seniors_string)
 
 remaining_number_sessions_non_seniors = 0
 remaining_dates_everyone_else = [
-4/4, 4/5, 4/6, 4/7, 
-4/11, 4/12, 4/13, 4/14, 
-4/18, 4/19, 4/20, 4/21, 
-4/25, 4/26, 4/27, 4/28,
-5/2, 5/3, 5/4, 5/5,
-5/9, 5/10, 5/11, 5/12,
-5/16, 5/17, 5/18, 5/19,
+'4-4', '4-5', '4-6', '4-7', 
+'4-11', '4-12', '4-13', '4-14', 
+'4-18', '4-19', '4-20', '4-21', 
+'4-25', '4-26', '4-27', '4-28',
+'5-2', '5-3', '5-4', '5-5',
+'5-9', '5-10', '5-11', '5-12',
+'5-16', '5-17', '5-18', '5-19'
 ] 
+print(remaining_dates_everyone_else)
+
 remaining_number_sessions_non_seniors = len(remaining_dates_everyone_else)
-print ("number of sessions left for non-seniors: ", remaining_number_sessions_non_seniors)
+# print ("number of sessions left for non-seniors: ", remaining_number_sessions_non_seniors)
+remaining_dates_everyone_else_string=''
+remaining_dates_everyone_else_string=''.join([str(item) + ", " for item in remaining_dates_everyone_else])
+
+remaining_dates_everyone_else_string_stripped=remaining_dates_everyone_else_string[:-2]
+print (remaining_dates_everyone_else_string_stripped)
 
 # Step 1: process attendance data for non-seniors and insert attnum, skipnum, attrate,  
 # remaining_number_sessions_non_seniors, and neednum_everyone_else into list
@@ -129,10 +128,11 @@ for inner_list in everyone_else:
             neednum_everyone_else = neednum_everyone_else + 1
             inner_list.insert(6, neednum_everyone_else)
 # write the student data to txt report file
-            entry = "You must attend at least " + str(neednum_everyone_else) + " sessions in order to reach the critical threshold of the majority of practices (i.e. an attendance rate of more than 50%) and pass Spring Fitness." + '\n'
-            email_report += entry
-            email_report += str(inner_list) + '\n\n'
-            break
+            entry = "Dear " + inner_list[0] + ",\n\n    Greetings from Mr. Reitz' Python3 attendancebot. You still need to attend at least " + str(inner_list[6]) + " practices of the remaining " + str(inner_list[5]) + " in the school year in order to reach the critical threshold of the majority of practices (i.e. an attendance rate of more than 50%) and pass Spring Fitness. Your remaining practice dates are: " + remaining_dates_everyone_else_string_stripped + ".\n\nIn addition there will be two optional make-up practices on 5/23 and 5/24. \n\nIf you have any questions about this automated attendance notification, please reach out to my programmer, Mr Reitz, at breitz@saes.org \n\nSincerely, \nMr Reitz' Python3 Attendancebot \n\n"
+            email_report += str(entry)
+            inner_list.insert(7, email_report)
+            email_report += "\n"
+            break     
 # Step 2: process attendance data for seniors and insert attnum, skipnum, attrate,  
 # remaining_number_sessions_non_seniors, and neednum_everyone_else into list
 
@@ -167,9 +167,10 @@ for inner_list in seniors:
             neednum_seniors = neednum_seniors + 1
             inner_list.insert(6, neednum_seniors) 
 # write the student data to txt report file        
-            entry = "You must attend at least " + str(neednum_seniors) + " sessions in order to reach the critical threshold of the majority of practices (i.e. an attendance rate of more than 50%) and pass Spring Fitness." + '\n'
-            email_report += entry
-            email_report += str(inner_list) + '\n\n'
+            entry = "Dear " + inner_list[0] + ",\n\n   Greetings from Mr. Reitz' Python3 attendancebot. You still need to attend at least " + str(inner_list[6]) + " practices of the remaining " + str(remaining_number_sessions_seniors) + " in the school year in order to reach the critical threshold of the majority of practices (i.e. an attendance rate of more than 50%) and pass Spring Fitness. Your remaining practice dates are: " + remaining_dates_sessions_seniors_string_stripped + ". \n\nIn addition there will be four optional/make-up practices on: 5-9, 5-10, 5-11, 5-12. \n\nIf you have any questions about this automated attendance notification, please reach out to my programmer, Mr Reitz, at breitz@saes.org \n\nSincerely, \nMr Reitz' Python3 Attendancebot \n\n"
+            email_report += str(entry)
+            inner_list.insert(7, email_report)
+            email_report += "\n"
             break             
 
 #create a txt report that includes all students with the email message to be sent
@@ -179,4 +180,3 @@ with open('report.txt', 'w') as file:
 #email each student with customized email message
 
 #create an end of year report with attendance records and a list of passing students
-
--- test.py
+++ test.py
@@ -1,12 +1,4 @@
-remaining_number_sessions_non_seniors = 28
-neednum_everyone_else = 0
-attnum = 4
-skipnum = 4
-
-neednum_everyone_else = remaining_number_sessions_non_seniors
-while neednum_everyone_else >= 0:
-    neednum_everyone_else = neednum_everyone_else - 1
-    if (attnum+neednum_everyone_else)/(attnum+skipnum+remaining_number_sessions_non_seniors) <= 0.5:
-        break
-    print(neednum_everyone_else)
-print("loop is finished")
+list=['a', 'b', 'c', 'd']
+string=''.join([str(item) + " " for item in list])
+print("converting list into string \n")
+print(string)
